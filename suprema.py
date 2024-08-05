98f02a81c1b4307d498016f7be0fdd1b5c26bb07
Vinícius Campos <163905335+Vipires19@users.noreply.github.com>
2024-08-05 12:30:24 -0300

    Add files via upload

--- suprema.py
+++ suprema.py
@@ -8,6 +8,7 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import urllib
 import urllib.parse
+from datetime import datetime
 
 st.set_page_config(
             layout =  'wide',
@@ -23,11 +24,12 @@ client = MongoClient("mongodb+srv://%s:%s@cluster0.gjkin5a.mongodb.net/?retryWri
 st.cache_resource = client
 db = client.supremasat
 coll = db.veiculos
+coll2 = db.pagamentos
 
 def cadastrar_veiculos():
     col1,col2,col3,col4,col5,col6 = st.columns(6)
     placa_lt = col1.text_input('Placa', placeholder = 'Insira as letras da placa do veículo')
-    placa_nb = col2.text_input('', placeholder = 'Insira a segunda parte da placa')
+    placa_nb = col2.text_input('Placa2', placeholder = 'Insira a segunda parte da placa',label_visibility="hidden")
     modelo = col3.text_input('Modelo', placeholder = 'Insira o modelo do veículo')
     cor = col4.text_input('Cor', placeholder = 'Insira a cor do veículo')
     ano = col5.number_input('Ano',min_value= 0, max_value= 9999,value=None, placeholder = 'Insira o ano do veículo')
@@ -53,9 +55,8 @@ def cadastrar_veiculos():
         entry = [veiculo]
         result = coll.insert_many(entry)
 
-def pagina_principal():
-    st.title('Suprema Sat 🌎')
-    cadastrar_veiculos()
+def visualizar_veiculos():
+    
     clientes = db.veiculos.find({})
 
     clientesdf = []
@@ -64,7 +65,77 @@ def pagina_principal():
 
     df = pd.DataFrame(clientesdf, columns= ['_id', 'Cliente','Documento','N° Doc', 'Veículo', 'Placas', 'Cor', 'Ano', 'Tipo'])
     df.drop(columns='_id', inplace=True)
+    st.session_state['veiculos'] = df
+    df = st.session_state['veiculos']
+    
     st.dataframe(df)
+
+def pagamento_veiculos():
+    df = st.session_state['veiculos']
+    cliente = df['Cliente'].value_counts().index
+    clientes = st.selectbox('Cliente', cliente)
+    df_cliente = df[df['Cliente'] == clientes]
+    col1,col2,col3,col4,col5 = st.columns(5)
+    doc = df_cliente['Documento'].value_counts().index[0]
+    col1.metric('Documento', doc)
+    num = df_cliente['N° Doc'].value_counts().index[0]
+    col2.metric('num', num, label_visibility="hidden")
+    tipo = df_cliente['Tipo'].value_counts().index[0]
+    col3.metric('Tipo', tipo)    
+    veic = df_cliente['Veículo'].value_counts().index[0]
+    col4.metric('Modelo', veic)
+    placa = df_cliente['Placas'].value_counts().index[0]
+    col4.metric('Placa', placa)
+    cor = df_cliente['Cor'].value_counts().index[0]
+    col5.metric('Cor', cor)    
+    ano = df_cliente['Ano'].value_counts().index[0]
+    col5.metric('Ano', ano)
+    forma_pgto = ['Dinheiro', 'Pix', 'Cartão Débito', 'Cartão Crédito']
+    pagou = col1.selectbox('Forma de pagamento', forma_pgto)
+    valor = col2.number_input('Valor', placeholder = 'Valor em R$')
+    confirma_pgto = col1.button('Confirmar pagamento')
+    
+    if confirma_pgto:
+        data = str(datetime.now().date())
+        pgto = {'Cliente' : clientes,
+                'Documento' : num,
+                'Data' : data,
+               'Forma Pagamento': pagou ,
+               'Valor' : valor,
+               'Status' : 'Confirmado'}
+        entry = [pgto]
+        result = coll2.insert_many(entry)
+
+def visu():
+    pagamentos = db.pagamentos.find({})
+    pagamentosdf = []
+    for item in pagamentos:
+        pagamentosdf.append(item)
+
+    dfpgto = pd.DataFrame(pagamentosdf, columns= ['_id', 'Cliente','Documento','Data', 'Forma Pagamento', 'Valor', 'Status'])
+    dfpgto.drop(columns='_id', inplace=True)
+    st.session_state['pagamentos'] = dfpgto
+    dfpgto = st.session_state['pagamentos']
+    dfveic = st.session_state['veiculos']
+    df_pagamento = pd.merge(dfveic, dfpgto, on="Cliente", how='outer')
+    df_pagamento.drop(columns= 'Documento_y', inplace=True)
+    st.session_state['df_pagamento'] = df_pagamento
+    df = st.session_state['df_pagamento']
+    st.dataframe(df)
+
+def pagina_principal():
+    st.title('Suprema Sat 🌎')
+    cadastrar_veiculos()
+    visualizar_veiculos()
+    
+    st.divider()
+
+    st.header('**Pagamento**')
+    pagamento_veiculos()
+
+    st.divider()
+    st.header('**Visulizando pagamentos**')
+    visu()
 
 def main():
     pagina_principal()
